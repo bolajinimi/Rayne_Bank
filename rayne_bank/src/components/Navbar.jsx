96d11155133156ea4ced726efe56f6fe495fd975
Olatunji Clement <clement.olatunji@decagon.dev>
2023-12-12 09:38:39 +0100

    rayne_bank

--- rayne_bank/src/components/Navbar.jsx
+++ rayne_bank/src/components/Navbar.jsx
@@ -6,6 +6,8 @@ const Navbar = () => {
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
 
+  
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
